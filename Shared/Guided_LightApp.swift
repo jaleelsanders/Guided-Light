ca8ea43c872d49b7e4ef77a144f36a6dacc88e13
jaleelsanders <jaleelsanders96@gmail.com>
2021-07-08 10:29:59 -0500

    Initial Commit

---
+++ Shared/Guided_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guided_LightApp.swift
+//  Shared
+//
+//  Created by jaleel sanders on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Guided_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
